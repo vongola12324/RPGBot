fd9b0a7da8ba5d6345e84b0ca93b8303b834ea6b
vongola12324 <vongola12324@coder.tw>
2017-04-24 15:40:58 +0800

    Update adminModel

--- Web/admin.py
+++ Web/admin.py
@@ -6,23 +6,17 @@ from .models import *
 # ModelAdmins
 class ProfileAdmin(admin.ModelAdmin):
     model = Profile
-    list_display = ["Name",]
-
-    def Name(self, instance):
-        return instance.user.username
+    list_display = ["user", "name"]
 
 
 class PetStatusAdmin(admin.ModelAdmin):
     # list_display = ["狀態名稱", "狀態描述", "無法攻擊機率", "攻擊同隊機率", "攻擊自己機率", "攻擊修正", "防禦修正"]
-    list_display = ["Name", ]
-
-    def Name(self, instance):
-        return instance.name
+    list_display = ["name", "desc"]
 
 
 class PetSkillAdmin(admin.ModelAdmin):
     # list_display = ["技能名稱", "技能描述", "血量修正", "攻擊修正", "防禦修正", "直接攻擊值", "直接攻擊機率"]
-    list_display = ["Name", ]
+    list_display = ["name", "desc"]
 
     def Name(self, instance):
         return instance.name
@@ -30,17 +24,14 @@ class PetSkillAdmin(admin.ModelAdmin):
 
 class PetPrototypeAdmin(admin.ModelAdmin):
     # list_display = ["原型名稱", "故事", "最大血量", "最大攻擊", "最大防禦", "基礎血量", "基礎攻擊", "基礎防禦", "可習得技能", "最大等級"]
-    list_display = ["Name", ]
+    list_display = ["name", ]
 
-    def Name(self, instance):
-        return instance.name
 
 class PetAdmin(admin.ModelAdmin):
     # list_display = ["擁有者", "寵物原型", "等級", "狀態"]
-    list_display = ["Name", ]
+    list_display = ["player", "proto", "level"]
 
-    def Name(self, instance):
-        return instance.name
+
 
 # Register your models here.
 # admin.site.register(Profile, ProfileAdmin)
